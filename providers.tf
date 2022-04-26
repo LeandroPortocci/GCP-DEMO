3924fe56f6698e5123da2a9bdc5209c45afca0ed
leandro luiz portocci <leandro.portocci@kyndryl.com>
2022-04-26 17:26:42 -0300

    update gcp project

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@
 # corresponding provider blocks in configuration
 provider "google" {
   # version = "~> 3.77.0"
-  project = "devops-demo-347814"
+  project = "gcp-demo-348420"
   region  = "us-central1-c"
 }
 ####
